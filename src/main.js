cd7f7d5167baf3cf17593b35c43b167676088a28
joshnwosu <joshuanwosu078@gmail.com>
2022-06-02 00:20:13 +0100

    removed electron security warning

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+// Remove (Electron Security Warning)
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
   app.quit();
 }
@@ -14,7 +17,7 @@ const createWindow = () => {
     height: 600,
 
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
@@ -28,18 +31,18 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
@@ -50,8 +53,8 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-ipcMain.handle('say.hello', async (event, args) => {
+ipcMain.handle("say.hello", async (event, args) => {
   console.log(args);
 
-  return 'Hello from the main process: The app version is: ' + app.getVersion();
+  return "Hello from the main process: The app version is: " + app.getVersion();
 });
